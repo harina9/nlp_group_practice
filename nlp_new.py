1dc8e676ba7f67e9236dd0f2d3893a6ee7d8cf3d
Arina Petrova <petrova.arina.s@yandex.ru>
2021-12-05 13:31:55 +0300

    Adding with sklearn

--- nlp_new.py
+++ nlp_new.py
@@ -4,25 +4,32 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from utils import process_tweet
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 df = pd.read_table('reviews.tsv')
 
+"""Cut our dataframe and add new column with clean data"""
 small_df = df[:8000]
 small_df = small_df.drop(small_df[(small_df["rating"] == 0.0)].index)
 small_df.dropna(inplace=True)
 small_df["clean_text"] = small_df["review_text"].apply(process_tweet)
 print(small_df)
 
+"""Vectorization"""
 vectorizer = CountVectorizer()
 corpus = small_df['clean_text']
 
 X = vectorizer.fit_transform(corpus)
 y = small_df["rating"]
 
+"""Split our dataset into train and test"""
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
 
+"""Build model"""
 clf = LogisticRegression(random_state=0).fit(X_train, y_train)
 
-y_pred = clf.predict(X_test)
-print(accuracy_score(y_test, y_pred))
+"""Prediction"""
+y_prediction = clf.predict(X_test)
+print(accuracy_score(y_test, y_prediction))
+print(confusion_matrix(y_test, y_prediction))
+
